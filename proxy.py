7e6a71eb462734d55800bcba538f4f7f2ee21ec4
Alex Morkovkin <alex1080gm@gmail.com>
2020-08-02 01:40:24 +0500

    Initial commit

---
+++ proxy.py
@@ -0,0 +1,55 @@
+import requests
+import json
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+host = 'www.google.com'
+
+
+@app.route('/', defaults={'path': ''}, methods=['GET'])
+@app.route('/<path:path>', methods=['GET'])
+def catch_all_get(path):
+    url = create_url(path)
+
+    try:
+        response = requests.get(url)
+    except Exception as error:
+        print("ERROR", error)
+    finally:
+        print(url)
+
+    if 'json' in response.headers["content-type"]:
+        return json.dumps(json.loads(response.content.decode('utf-8')), ensure_ascii=False)
+    else:
+        return response.content
+
+
+@app.route('/', defaults={'path': ''}, methods=['POST'])
+@app.route('/<path:path>', methods=['POST'])
+def catch_all_post(path):
+    url = create_url(path)
+
+    try:
+        response = requests.post(url, data=request.json)
+    except Exception as error:
+        print("ERROR ", error)
+    finally:
+        print(url)
+        print(" with body {body}".format(body=request.json))
+
+    return response.json()
+
+
+def create_url(path):
+    url = 'https://{host}/{path}?{query_string}'.format(
+        host=host,
+        path=path,
+        query_string=request.query_string.decode('ascii')
+    )
+    return url
+
+
+if __name__ == '__main__':
+    app.run()
